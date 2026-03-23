e4161fb8095711f2442cca3d5e1bbd04ad23a8af
Aabir Dutta <aabir.dutta.04@gmail.com>
2026-03-24 00:39:23 +0530

    Added Classification Report to Eva_Utils module

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,6 +1,6 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 
-def evaluate_model(true_labels, predictions):
+def evaluate_model(true_labels, predictions, target_names=None):
 
     acc = accuracy_score(true_labels, predictions)
     precision = precision_score(true_labels, predictions)
@@ -8,12 +8,20 @@ def evaluate_model(true_labels, predictions):
     f1 = f1_score(true_labels, predictions)
     cm = confusion_matrix(true_labels, predictions)
 
+    report = classification_report(
+        true_labels,
+        predictions,
+        target_names=target_names,
+        digits=4
+    )
+
     results = {
         "Accuracy": acc,
         "Precision": precision,
         "Recall": recall,
         "F1 Score": f1,
-        "Confusion Matrix": cm
+        "Confusion Matrix": cm,
+        "Classification Report": report
     }
 
     return results
